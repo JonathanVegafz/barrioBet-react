9b5ef2bbd2522aa1de8bc46cdc64635f7e8644bd
Chespirito133 <jonathan.vega.f@mail.pucv.cl>
2023-08-21 21:58:01 -0400

    dfs

--- src/cuadroEquipo.js
+++ src/cuadroEquipo.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from './logo.svg';
 
-export const CuadroEquipo = () =>{
+export const CuadroEquipo = ({titulo, logo, nombreEquipo}) =>{
     return(
         <div className="cuadro">
-            <h1>Local</h1>
+            <h1>{titulo}</h1>
             <img src={logo} className="logoEquipo"></img>
-            <h1>Nombre Equipo</h1>
+            <h1>{nombreEquipo}</h1>
         </div>
     )
 }
